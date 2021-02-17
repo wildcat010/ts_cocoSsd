ffa1b0216c248fc719b8dcbe5deb7fe1e98d3ff4
Benoit <benoit@cpe-08-02-rs.krapod.hor.net.pl>
2021-02-17 20:22:52 +0100

    modification to make it responsive

--- src/components/DrawScene.js
+++ src/components/DrawScene.js
@@ -2,10 +2,12 @@
 
 export function DrawScene(element) {
   const surface = document.getElementById('imageToProcess');
-
   const e = document.createElement("div");
   e.setAttribute("class", "prediction");
-  e.style = "position:absolute;border:1px solid green;z-index:1;margin-left: "+element.bbox[0]+"px; margin-top: "+element.bbox[1]+"px; left:0;top:0;width:"+element.bbox[2]+"px;height:"+element.bbox[3]+"px;";
+
+  let left = (window.innerWidth - surface.children[0].clientWidth)/2;
+
+  e.style = "position:absolute;left:"+left+"px;border:1px solid green;z-index:1;margin-left: "+element.bbox[0]+"px; margin-top: "+element.bbox[1]+"px;width:"+element.bbox[2]+"px;height:"+element.bbox[3]+"px;";
 
   surface.appendChild(e);
 
